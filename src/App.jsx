cb765090e8819dd5b470a323bb1648335e64699e
youjunho613 <njn613@gmail.com>
2023-06-14 20:04:21 +0900

    initial. git commit 재구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { React, useState } from "react";
-import Header from "component/header";
-import Form from "component/form";
-import TodoList from "component/todoList";
-import "style/App.css";
+import React from "react";
 
-function App() {
-  const [todoList, setTodoLIst] = useState([
-    { title: "", content: "", id: 1, isDone: false },
-  ]);
-
-  return (
-    <div className="html-style">
-      <Header>My Todo List</Header>
-      <Form todoList={todoList} setTodoLIst={setTodoLIst} />
-      <TodoList todoList={todoList} setTodoLIst={setTodoLIst} />
-    </div>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
--- src/component/header.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Header = ({ children }) => {
-  return <h1 className="body-title">{children}</h1>;
-};
-
-export default Header;
