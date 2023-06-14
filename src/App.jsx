d211d66f36550a8bacc9bd5910dfc3f952698a50
youjunho613 <njn613@gmail.com>
2023-06-14 20:41:13 +0900

    feat. add submit event handler
    create state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import { React, useState } from "react";
 import Header from "component/header";
 import Form from "component/form";
 import Main from "component/main";
 import "App.css";
 
 const App = () => {
+  const [todoList, setTodoLIst] = useState([
+    { title: "", content: "", id: 1, isDone: false },
+  ]);
+
   return (
     <div className="html-style">
       <Header>My Todo List</Header>
-      <Form></Form>
-      <Main></Main>
+      <Form todoList={todoList} setTodoLIst={setTodoLIst} />
+      <Main />
     </div>
   );
 };
--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 
-const Form = () => {
+const Form = ({ todoList, setTodoLIst }) => {
+  const initial = { title: "", content: "", id: 1, isDone: false };
+  const [todo, setTodo] = useState(initial);
+
+  // id 값 부여
+  const todoId = todoList.length > 0 ? todoList[todoList.length - 1].id + 1 : 1;
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setTodoLIst([...todoList, { ...todo, id: todoId }]);
+    setTodo(initial);
+  };
+
+  const inputTag = (name) => ({
+    className: "write-input",
+    name,
+    value: todo[name],
+    onChange: onChangeHandler,
+  });
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+
+    setTodo({ ...todo, [name]: value });
+  };
+
   return (
-    <form className="write-form">
-      제목
-      <input className="write-input" />
-      내용
-      <input className="write-input" />
-      <button className="write-button">추가하기</button>
+    <form className="write-form" onSubmit={onSubmitHandler}>
+      제목 <input {...inputTag("title")} />
+      내용 <input {...inputTag("content")} />
+      <button className={"write-button"}>추가하기</button>
     </form>
   );
 };
