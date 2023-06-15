ea5601d8dc79861468c326b8ad1dccd80895bf3b
youjunho613 <njn613@gmail.com>
2023-06-15 10:04:04 +0900

    fix. 오타 수정

--- src/page/main.jsx
+++ src/page/main.jsx
@@ -1,16 +1,15 @@
-import React from "react";
-import Todo from "../component/todo";
+import Todo from "../component/Todo";
 
 const Main = ({ todoList, setTodoList }) => {
   return (
     <main>
       <h2 className="list-title">Ing..</h2>
       <ul className="do-list">
-        <Todo todoList={todoList} setTodoLIst={setTodoList} boolean={false} />
+        <Todo todoList={todoList} setTodoList={setTodoList} boolean={false} />
       </ul>
       <h2 className="list-title">Done..</h2>
       <ul className="do-list">
-        <Todo todoList={todoList} setTodoLIst={setTodoList} boolean={true} />
+        <Todo todoList={todoList} setTodoList={setTodoList} boolean={true} />
       </ul>
     </main>
   );
