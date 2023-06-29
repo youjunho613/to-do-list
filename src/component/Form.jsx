5a37847d2d64c5200115c39212144a2a3f90a954
youjunho613 <njn613@gmail.com>
2023-06-29 22:55:50 +0900

    fix. 피드백 내용 수정

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo } from "redux/modules/todoList";
 import * as St from "./Form.style.js";
@@ -14,10 +14,6 @@ const Form = () => {
   const titleRef = useRef(""); // Ref로 인풋 태그 링크
   const contentRef = useRef("");
 
-  useEffect(() => {
-    titleRef.current.focus();
-  }, []); // title input 포커스 해주기 ---- Effect로 렌더링 될 때 한번만 실행
-
   const onChangeHandler = event => {
     const { name, value } = event.target;
     setTodo({ ...todo, [name]: value });
@@ -25,14 +21,11 @@ const Form = () => {
 
   const onSubmitHandler = event => {
     event.preventDefault();
-    if (titleRef.current.value === "" || contentRef.current.value === "") return; // input 유효성 검사
     const todoId = todoList.length > 0 ? todoList[todoList.length - 1].id + 1 : 1; // id 값 유효성
 
     dispatch(addTodo({ ...todo, id: todoId })); // 모듈로 추가 이벤트 넘겨서 구현
 
     setTodo(INITIAL); // input 값 초기화
-
-    titleRef.current.focus(); // input 커서 위치
   };
 
   const inputTag = name => ({
@@ -44,9 +37,9 @@ const Form = () => {
   return (
     <St.Form onSubmit={onSubmitHandler}>
       <St.Label htmlFor="title">제목</St.Label>
-      <St.Input {...inputTag("title")} ref={titleRef} />
+      <St.Input {...inputTag("title")} ref={titleRef} autoFocus required />
       <St.Label htmlFor="content">내용</St.Label>
-      <St.Input {...inputTag("content")} ref={contentRef} />
+      <St.Input {...inputTag("content")} ref={contentRef} required />
       <St.Button>추가하기</St.Button>
       <button type={"radio"}> </button>
     </St.Form>
